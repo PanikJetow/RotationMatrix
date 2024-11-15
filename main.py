7735b28aa819811400acca760aade8fc3f80cb54
PanikJetow <eleminati3.0@gmail.com>
2024-11-15 23:28:46 +0100

    added cube generation, can rotate

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-import math
+import numpy as np
 
 def main():
-	print(buildCube([1,1,1], 10))
+	shape_vertices = (buildCube([0,0,0], 20))
+	print(shape_vertices)
+	print(rotate(shape_vertices, 0, 180, 0))
 	return 0
 
 def buildCube(start_coord, edge_length):
 	try:
+		vectors = [[edge_length,0,0],[0,edge_length,0],[0,0,edge_length]]
 		cube = []
-		for x1 in range(start_coord[0], start_coord[0] + edge_length):
-			for x2 in range(start_coord[1], start_coord[1] + edge_length):
-				for x3 in range(start_coord[2], start_coord[2] + edge_length):
-					cube.append([x1, x2, x3])
-		return cube
-	except:
-		if len(start_coord) != 3:
-			print(f"Error: start_coord must be a list of 3 integers, not {type(start_coord)}")
-			return None
-		elif edge_length < 0 or type(edge_length) != int:
-			print(f"Error: edge_length must be a positive integer, not {type(edge_length)}")
+		cube.append(np.array(start_coord))
+		cube.append(np.array(start_coord) + np.array(vectors[0]))
+		cube.append(np.array(start_coord) + np.array(vectors[0]) + np.array(vectors[1]))
+		cube.append(np.array(start_coord) + np.array(vectors[1]))
+
+		cube.append(np.array(start_coord) + np.array(vectors[2]))
+		cube.append(np.array(start_coord) + np.array(vectors[0]) + np.array(vectors[2]))
+		cube.append(np.array(start_coord) + np.array(vectors[0]) + np.array(vectors[1]) + np.array(vectors[2]))
+		cube.append(np.array(start_coord) + np.array(vectors[1]) + np.array(vectors[2]))
+
+		return np.array(cube)
+	except Exception as exception:
+		print(exception)
+		#if len(start_coord) != 3:
+		#	print(f"Error: start_coord must be a list of 3 integers, not {type(start_coord)}")
+		#	return None
+		#elif edge_length < 0 or type(edge_length) != int:
+		#	print(f"Error: edge_length must be a positive integer, not {type(edge_length)}")
+
+def rotate(vertices, a = 0, b = 0, c = 0):
+	a = np.deg2rad(a)
+	b = np.deg2rad(b)
+	c = np.deg2rad(c)
+	sina = np.sin(a); cosa = np.cos(a); sinb = np.sin(b); cosb = np.cos(b); sinc = np.sin(c); cosc = np.cos(c)
+
+	R = np.array([[cosa * cosb, cosa * sinb * sinc - sina * cosc, cosa * sinb * cosc + sina * sinc], [sina * cosb, sina * sinb * sinc + cosa * cosc, sina * sinb * cosc - cosa * sinc], [-sinb, cosb * sinc, cosb * cosc]])
+	rotated = np.dot(vertices, R)
+	return rotated
+
 
 if __name__ == "__main__":
 	main()
-test
