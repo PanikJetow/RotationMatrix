da98e0e05dd64b81f32b541a8bbfb4c3e485f5aa
PanikJetow <eleminati3.0@gmail.com>
2024-11-17 15:49:01 +0100

    sync

--- main.py
+++ main.py
@@ -36,15 +36,7 @@ def main():
 	pygame.quit()
 	return 0
 
-def rotate(shape_object: np.ndarray, a = 0, b = 0, c = 0) -> np.ndarray:
-	a = np.deg2rad(a)
-	b = np.deg2rad(b)
-	c = np.deg2rad(c)
-	sina = np.sin(a); cosa = np.cos(a); sinb = np.sin(b); cosb = np.cos(b); sinc = np.sin(c); cosc = np.cos(c)
 
-	R = np.array([[cosa * cosb, cosa * sinb * sinc - sina * cosc, cosa * sinb * cosc + sina * sinc], [sina * cosb, sina * sinb * sinc + cosa * cosc, sina * sinb * cosc - cosa * sinc], [-sinb, cosb * sinc, cosb * cosc]])
-	shape_object.vertices = np.dot(shape_object.vertices, R)
-	return shape_object
 
 
 if __name__ == "__main__":
--- shapes.py
+++ shapes.py
@@ -21,3 +21,13 @@ class Shape:
             return self
         except Exception as exception:
             print(exception)
+
+    def rotate(self, a = 0, b = 0, c = 0) -> np.ndarray:
+        a = np.deg2rad(a)
+        b = np.deg2rad(b)
+        c = np.deg2rad(c)
+        sina = np.sin(a); cosa = np.cos(a); sinb = np.sin(b); cosb = np.cos(b); sinc = np.sin(c); cosc = np.cos(c)
+
+        R = np.array([[cosa * cosb, cosa * sinb * sinc - sina * cosc, cosa * sinb * cosc + sina * sinc], [sina * cosb, sina * sinb * sinc + cosa * cosc, sina * sinb * cosc - cosa * sinc], [-sinb, cosb * sinc, cosb * cosc]])
+        self.vertices = np.dot(self.vertices, R)
+        return self
